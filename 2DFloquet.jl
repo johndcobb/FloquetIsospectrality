0c897e88643b98f44fbab1bfc3d8051cdd6d7e08
John Cobb <jdcobb3@gmail.com>
2025-10-15 13:51:05 -0500

    implement 2D case

--- 2DFloquet.jl
+++ 2DFloquet.jl
@@ -1,8 +1,8 @@
 using HomotopyContinuation
 
-@var x[1:9]
+@var x[1:9] y[1:4]
 
 E =  include("./cache/E3x3.jl")
 
-F = System([E])
-res = solve(F)
+F = System(E)
+res = solve(F; start_system =:total_degree)
